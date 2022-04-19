b22eeeab07b1530c4cdeeb9abd275a8e487a1fd4
Iuri Adriel <iuri.adriel@gmail.com>
2022-04-19 20:40:29 -0300

    Projeto iniciado.

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <!-- <div><button type="button" @click="increment">clicou</button> CONTADOR: {{ counter }}</div> -->
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+// import { useCounterStore } from 'src/stores/example-store'
+// import { mapActions, storeToRefs } from 'pinia'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup(){
+    // const counterStore = useCounterStore();
+    // const { counter } = storeToRefs(counterStore);
+    // const { increment } = mapActions(useCounterStore, ["increment"]);
+
+
+
+    return {
+      // counter,
+      // increment
+    }
+  }
+})
+</script>
